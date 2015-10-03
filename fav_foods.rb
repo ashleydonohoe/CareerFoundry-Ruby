2d64c3f12afa72317c82a047c33ba290a10cdbd6
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-10-02 22:55:50 -0400

    updated questions

--- fav_foods.rb
+++ fav_foods.rb
@@ -12,8 +12,12 @@ fav_foods
 
 # Questions
 
-#The map method on an array performs an operation on each item in the array and returns the updated values without changing the array itself (unless a bang is used like map!). An example of its use with an array would be: arr.map { |a| a* 3} on an array of numbers called "arr".
+#The map method on an array performs an operation on each item in the array and returns the updated values without changing the array itself (unless a bang is used like map!). An example of its use with an array would be: 
+arr = [1, 2, 3, 4, 5]
+arr.map { |a| a* 3} #Would return [3, 6, 9, 12, 15]
 
-#The include? method allows you to find out if an array contains a particular object. An example of its use would be: arr.include?("pizza"), which would return true if that array named "arr" contained "pizza".
+#The include? method allows you to find out if an array contains a particular object. An example of its use would be: 
+arr = ["pizza", "taco", "pasta", "fish"]
+arr.include?("pizza") #return true if that array named "arr" contained "pizza".
 
 #To access a value in a hash by its key, you use the hash name and then put the key name in brackets, such as my_hash[:key]. If the value for that key is an array, you can put an index value in brackets after the key in brackets, like my_hash[:key][0].
